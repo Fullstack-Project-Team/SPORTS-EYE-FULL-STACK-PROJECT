60e21063dc192c49a3c335492c1b208fd975dbca
LALIT <lalit.saini_cs19@gla.ac.in>
2021-10-10 20:51:03 +0530

    changes updated

--- football/script.js
+++ football/script.js
@@ -1,16 +1,13 @@
-var axios = require("axios").default;
-
-var options = {
-  method: 'GET',
-  url: 'https://api-football-v1.p.rapidapi.com/v3/timezone',
-  headers: {
-    'x-rapidapi-host': 'api-football-v1.p.rapidapi.com',
-    'x-rapidapi-key': '2e47a54e48msh4b039b0f5e8bb3ap1c4bddjsn1a7e415ab166'
-  }
-};
-
-axios.request(options).then(function (response) {
-	console.log(response.data);
-}).catch(function (error) {
-	console.error(error);
+fetch("https://api-football-v1.p.rapidapi.com/v3/timezone", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "api-football-v1.p.rapidapi.com",
+		"x-rapidapi-key": "2e47a54e48msh4b039b0f5e8bb3ap1c4bddjsn1a7e415ab166"
+	}
+})
+.then(response => {
+	console.log(response);
+})
+.catch(err => {
+	console.error(err);
 });
