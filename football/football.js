3b559428eea265c6f5d09d369372287c077f62b5
LALIT <lalit.saini_cs19@gla.ac.in>
2021-10-16 16:22:12 +0530

    team.html file in football section is added

--- football/football.js
+++ football/football.js
@@ -1,18 +1,20 @@
-fetch("https://api-football-v1.p.rapidapi.com/v3/timezone", {
+function fetchNews(){
+	fetch("https://api-football-v1.p.rapidapi.com/v3/timezone", {
 	"method": "GET",
 	"headers": {
 		"x-rapidapi-host": "api-football-v1.p.rapidapi.com",
 		"x-rapidapi-key": "2e47a54e48msh4b039b0f5e8bb3ap1c4bddjsn1a7e415ab166"
-	}
-})
-.then(response => {
-	console.log(response);
-	displayData(data)
-})
-.catch(err => {
-	console.error(err);
-});
+		}
+	}).then(response => {
+		console.log(response);
+		//displayData(data)
+	}).catch(err => {
+		console.error(err);
+	});
+}
 
-function displayData(data) {
-	const cocktailDiv = document.getElementById("football");
-  }  
+fetchNews();
+
+// function displayData(data) {
+// 	var footballDiv = document.getElementById("football");
+// }
